5e7d42f249cdb677dda7079448d4b086ad842638
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2024-05-16 22:26:52 -0300

    main / - primera entrega

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-
-
 /*****PAGINA DE PESO Y BALANCEO DE LA AERONAVE C206 PARA 6 PERSONAS****/
-
 /* Se solicita el ingreso de cantidad de pasajeros y tripulacion(maximo 2 personas), que en total no pueden superar las 6 personas
 Luego se solicita el peso con valijas de cada persona y finalmente la distancia a recorrer
-
 El script calcula a partir de los datos entregados la cantidad de combustible necesaria a cargar para realizar la navegacion 
 y si es posible cargar la aeronave con ese peso sin superar el peso maximo de despegue-
 
@@ -14,21 +10,50 @@ PESO VACIO: 1989 lbs
 COMBUSTIBLE MAXIMO: 92 galones
 
 13.6 gph a 142 kts
-
 */
-tripulantes();
+let tripulacion = tripulantes();
+console.log(tripulacion);
+let pas = pasajeros()
+console.log(pas);
 
-let pasajeros = prompt("Ingrese la cantidad de pasajeros");
 
 
 function tripulantes(){
-    let tripulantes = prompt("Ingrese la cantidad de tripulantes");
-    checktrip(tripulantes);
+    let trips = prompt("Ingrese la cantidad de tripulantes");
+    if(isNaN(trips)){
+        alert("Error al ingresar el numero de tripulantes");
+        tripulantes();
+        
+    } else if(trips <= 0){
+        alert("Se debe ingresar al menos un tripulante");
+        tripulantes();
+    }else if(trips > 2){
+        alert("No se pueden ingresar mas de dos tripulantes");
+        tripulantes();
+    }else{
+        return parseInt(trips);
+    }
+}
+function reingresoTrip(){
+
 }
 
-function checktrip(tripulantes){
-    if(tripulantes <= 0){
-        alert("Error al ingresar los tripulantes");
-        tripulantes();
-    } //no puede ser cero y debe ser un numero
+function pasajeros(){
+    let pax = prompt("Ingrese la cantidad de pasajeros");
+    return checkPax(pax);
+}
+
+function checkPax(pax){ // pax debe ser un numero mayor o igual a cero y menor o igual a 4
+    if(isNaN(pax)){
+        alert("Error al ingresar el numero de pasajeros");
+        pasajeros();
+    }else if(pax < 0){
+        alert("No pueden ingresarse valores negativos");
+        pasajeros();
+    }else if((tripulacion === 2 && pax >4)||(tripulacion === 1 && pax>5)){
+        alert("Se deben ingresar menos pasajeros")
+        pasajeros();
+    }else{
+        return parseInt(pax);
+    }
 }
